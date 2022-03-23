7fe0b91b51bf132227e7f673700d84a84c1368c0
UNNEW-main <1970005826c@gmail.com>
2022-03-24 00:19:39 +0900

    초기화작업

--- src/Services/modules/users/fetchOne.ts
+++
@@ -1,7 +0,0 @@
-import { EndpointBuilder } from '@reduxjs/toolkit/dist/query/endpointDefinitions'
-import { User } from '.'
-
-export default (build: EndpointBuilder<any, any, any>) =>
-  build.query<User, string>({
-    query: id => `/users/${id}`,
-  })
