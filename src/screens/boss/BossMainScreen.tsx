3062dd9c426ae7f39db166ffa393e0084fe2fac7
UNNEW-main <1970005826c@gmail.com>
2022-05-02 10:39:52 +0900

    style: BossScreen ScrollView적용

--- src/screens/boss/BossMainScreen.tsx
+++ src/screens/boss/BossMainScreen.tsx
@@ -2,7 +2,7 @@ import { RegisterStoreButton } from '@/apps/boss/components/RegisterStoreButton'
 import { GoogleLogoutbutton, SignoutButton } from '@/components'
 import { StoreType } from '@/types/storeType'
 import React from 'react'
-import { Text, View } from 'react-native'
+import { ScrollView, Text, View } from 'react-native'
 import styled from 'styled-components/native'
 
 type BossMainScreenProps = {
@@ -10,59 +10,63 @@ type BossMainScreenProps = {
 }
 export const BossMainScreen = ({ list }: BossMainScreenProps) => {
   return (
-    <View
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '100%',
-      }}
-    >
-      <Text>보스 메인스크린</Text>
-      {list.length ? (
-        list.map((param, i) => (
-          <ItemContainer key={i}>
-            <Item>-----------------------</Item>
+    <Container>
+      <ScrollView
+        style={{
+          height: '100%',
+        }}
+      >
+        <Text>보스 메인스크린</Text>
+        {list.length ? (
+          list.map((param, i) => (
+            <ItemContainer key={i}>
+              <Item>-----------------------</Item>
 
-            <Item>{param.bossId}</Item>
-            <Item>{param.name}</Item>
-            <ItemContainer>
-              {param.businessHours.map((param, i) => (
-                <View key={i}>
-                  <Item>{param.days}</Item>
-                  <Item>{param.startTime}</Item>
-                  <Item>{param.endTime}</Item>
-                </View>
-              ))}
+              <Item>{param.bossId}</Item>
+              <Item>{param.name}</Item>
+              <ItemContainer>
+                {param.businessHours.map((param, i) => (
+                  <View key={i}>
+                    <Item>{param.days}</Item>
+                    <Item>{param.startTime}</Item>
+                    <Item>{param.endTime}</Item>
+                  </View>
+                ))}
+              </ItemContainer>
+              <Item>{param.category}</Item>
+              <Item>{param.description}</Item>
+              <ItemContainer>
+                {param.menus.map((param, i) => (
+                  <View key={i}>
+                    <Item>{param.menuCount}</Item>
+                    <Item>{param.price}</Item>
+                    <Item>{param.name}</Item>
+                  </View>
+                ))}
+              </ItemContainer>
+              <Item>{param.paymentMethods}</Item>
+              <Item>{param.storeId}</Item>
             </ItemContainer>
-            <Item>{param.category}</Item>
-            <Item>{param.description}</Item>
-            <ItemContainer>
-              {param.menus.map((param, i) => (
-                <View key={i}>
-                  <Item>{param.menuCount}</Item>
-                  <Item>{param.price}</Item>
-                  <Item>{param.name}</Item>
-                </View>
-              ))}
-            </ItemContainer>
-            <Item>{param.paymentMethods}</Item>
-            <Item>{param.storeId}</Item>
-          </ItemContainer>
-        ))
-      ) : (
-        <View>
-          <Text>가게가 없습니다!</Text>
-        </View>
-      )}
-      <RegisterStoreButton />
+          ))
+        ) : (
+          <View>
+            <Text>가게가 없습니다!</Text>
+          </View>
+        )}
+        <RegisterStoreButton />
 
-      <GoogleLogoutbutton />
-      <SignoutButton />
-    </View>
+        <GoogleLogoutbutton />
+        <SignoutButton />
+      </ScrollView>
+    </Container>
   )
 }
-
+const Container = styled.SafeAreaView`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+`
 const ItemContainer = styled.View`
   display: flex;
 `
