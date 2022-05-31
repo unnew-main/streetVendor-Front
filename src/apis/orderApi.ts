2dd35566eb234dff6c001774fe951542afa76927
UNNEW-main <1970005826c@gmail.com>
2022-05-31 21:43:42 +0900

    Feat: orderAPI 추가 Feat: orderType 추가

---
+++ src/apis/orderApi.ts
@@ -0,0 +1,17 @@
+import { OrderType } from '@/types/order.type'
+import api from './common'
+
+export const orderApi = {
+  userOrder: async (props: OrderType) => {
+    return api.postAuth('/api/v1/order', props)
+  },
+  bossCancelOrder: async (storeId: number, orderId: number) => {
+    return api.deleteAuth(`/api/v1/${storeId}/orders/${orderId}/cancel`, {})
+  },
+  userCancelOrder: async (orderId: number) => {
+    return api.deleteAuth(`/api/v1/orders/${orderId}/cancel`, {})
+  },
+  bossCheckOrder: async (storeId: number) => {
+    return api.getAuth(`/api/v1/orders/${storeId}`, {})
+  },
+}
---
+++ src/types/order.type.ts
@@ -0,0 +1,23 @@
+export type OrderType = {
+  distance: number
+  location: {
+    latitude: number
+    longitude: number
+  }
+  menus: [
+    {
+      count: number
+      menuId: number
+    },
+  ]
+  storeId: number
+}
+
+export type BasketType = {
+  menuId: number
+  menuCount: number
+  menuName: string
+  pictureUrl: string
+  menuPrice: number
+  menuTotal: number
+}
